a7f9b62bfa41c27d5684bb27eb1a1f32370728bd
nomadriding <fundora.a@gmail.com>
2024-08-22 14:43:05 -0400

    add onclick for the buttons lvl2 and lvl3

--- src/components/BuyCard.jsx
+++ src/components/BuyCard.jsx
@@ -4,8 +4,10 @@ export const BuyCard = ({ shells, buyCard, endTurn, setShowAvailableMoves }) => 
       {shells > 2 && (
         <button onClick={() => buyCard(1)}>Buy Level 1</button>
       )}
-      {shells > 7 && <button>Buy Level 2</button>}
-      {shells > 12 && <button>Buy Level 3</button>}
+      {shells > 7 && (
+        <button onClick={() => buyCard(2)}>Buy Level 2</button>)}
+      {shells > 12 && (
+        <button onClick={() => buyCard(3)}>Buy Level 3</button>)}
       <button
         onClick={() => {
           endTurn()

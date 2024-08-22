fa3c0edf42407b4c5b94d2d5640dbfa313687dbf
nomadriding <fundora.a@gmail.com>
2024-08-22 14:42:04 -0400

    add a few names to this card stack

---
+++ src/level2.jsx
@@ -0,0 +1,9 @@
+export const Level2 = [
+    {
+      name: "Thalassarus",
+      boardSlot: 6,
+      price: 8,
+      attackAction: { shells: 1, corals: 2 },
+      defenseAction: { shells:1, corals: 1 },
+    },
+]

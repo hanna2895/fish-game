555ee7edf5049a64d0df26914667437e8ef11521
nomadriding <fundora.a@gmail.com>
2024-08-22 14:41:38 -0400

    rename cards

--- src/level1.jsx
+++ src/level1.jsx
@@ -1,27 +1,27 @@
 export const Level1 = [
   {
-    name: "fish",
+    name: "Star Lumina",
     boardSlot: 1,
     price: 2,
     attackAction: { shells: 1 },
     defenseAction: { shells: 1 },
   },
   {
-    name: "fish2",
+    name: "Celesthydra",
     boardSlot: 2,
     price: 2,
     attackAction: { shells: 1 },
     defenseAction: { shells: 1 },
   },
   {
-    name: "fish3",
+    name: "Astranox",
     boardSlot: 2,
     price: 2,
     attackAction: { shells: 1 },
     defenseAction: { shells: 1 },
   },
   {
-    name: "Gold Fish",
+    name: "Goldfish",
     boardSlot: 3,
     price: 2,
     attackAction: { shells: 1 },

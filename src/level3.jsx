305ac6f9c0e6d10b5c2b357d7e0dc66f0cfaca3d
nomadriding <fundora.a@gmail.com>
2024-08-22 14:42:24 -0400

    add a few names to this card stack

---
+++ src/level3.jsx
@@ -0,0 +1,37 @@
+export const Level3 = [
+  {
+    name: "Thalassarus",
+    boardSlot: 8,
+    price: 13,
+    attackAction: { shells: 1, trident: 2 },
+    defenseAction: { shells:1, trident: 1 },
+  },
+  {
+    name: "Astral Marlin",
+    boardSlot: 9,
+    price: 12,
+    attackAction: { tridents: 3 },
+    defenseAction: { tridents: 2 },
+  },
+  {
+    name: "KrakenStar",
+    boardSlot: 10,
+    price: 14,
+    attackAction: { swap: 1 },
+    defenseAction: { swap: 1 },
+  },
+  {
+    name: "Sirenith",
+    boardSlot: 12,
+    price: 14,
+    attackAction: { tridents: 3 },
+    defenseAction: { tridents: 2 },
+  },
+  {
+    name: "SolaSquid",
+    boardSlot: 13,
+    price: 15,
+    attackAction: { shells: 3, corals: 1 },
+    defenseAction: { shells: 2, corals: 1 },
+  },
+];

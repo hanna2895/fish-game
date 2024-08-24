28c7ff5e58243500385facffd737ec0ede3515dd
nomadriding <fundora.a@gmail.com>
2024-08-24 10:36:49 -0400

    adjust to half price card

---
+++ src/components/Table.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Table = ({ table }) => {
+ 
+  return (
+    <div className="tableContainer">
+      {table.map((card, index) => {
+        const halfPrice = Math.round(card.price / 2)
+        return (
+          <div key={index} className="card centerCard">
+            <div className="cardName">{card.name}</div>
+            {card.price && <div>Price: {halfPrice}</div>}
+          </div>
+        )
+      })}
+    </div>
+  );
+};
+
+export default Table;

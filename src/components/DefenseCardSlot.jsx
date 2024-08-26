5f49e7401c28c91742a46c0b8c8f44e1c08cbddb
nomadriding <fundora.a@gmail.com>
2024-08-26 14:51:55 -0400

    modify defense card to stack

--- src/components/DefenseCardSlot.jsx
+++ src/components/DefenseCardSlot.jsx
@@ -1,20 +1,18 @@
 export const DefenseCardSlot = ({ slot, cards }) => {
+  const totalShells = cards.reduce((total, card) => total + (card.defenseAction.shells || 0), 0)
+  const totalCorals = cards.reduce((total, card) => total + (card.defenseAction.corals || 0), 0)
+  const totalTridents = cards.reduce((total, card) => total + (card.defenseAction.tridents || 0), 0)
+  const totalPearls = cards.reduce((total, card) => total + (card.defenseAction.pearls || 0), 0)
+
   return (
     <div className="cardSlot" id={`defenseCard${slot}`}>
-      {cards.map((card, index) => (
-        <div className="card cardDefense" key={index}>
-          <div className="cardName">{card.name}</div>
-          {card.defenseAction.shells && (
-            <div>shell {card.defenseAction.shells}</div>
-          )}
-          {card.defenseAction.corals && (
-            <div>corals {card.defenseAction.corals}</div>
-          )}
-          {card.defenseAction.tridents && (
-            <div>tridents {card.defenseAction.tridents}</div>
-          )}
+        <div className="card cardDefense">
+          <div className="cardName">Defense Card{cards.length !== 1 ? 's' : ''} {cards.length}</div>
+          {totalShells > 0 && <div>shells {totalShells}</div>}
+          {totalCorals > 0 && <div>shells {totalCorals}</div>}
+          {totalTridents > 0 && <div>shells {totalTridents}</div>}
+          {totalPearls > 0 && <div>shells {totalPearls}</div>}
         </div>
-      ))}
     </div>
   )
 }

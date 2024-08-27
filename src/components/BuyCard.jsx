57f5b2dfaee92d69e0ddfd8f537c56cf051f2539
nomadriding <fundora.a@gmail.com>
2024-08-26 23:11:56 -0400

    almost there with the defense card idea. Saving code.

--- src/components/BuyCard.jsx
+++ src/components/BuyCard.jsx
@@ -1,13 +1,13 @@
-export const BuyCard = ({ shells, buyCard, endTurn, setShowAvailableMoves }) => {
+export const BuyCard = ({ shells, buyCard, endTurn, setShowAvailableMoves, level1cards, level2cards, level3cards }) => {
     return (
       <div>
-      {shells > 2 && (
-        <button onClick={() => buyCard(1)}>Buy Level 1</button>
+      {shells > 2 && level1cards > 0 && (
+        <button onClick={() => buyCard(1)}>Buy Level 1 ({level1cards})</button>
       )}
-      {shells > 7 && (
-        <button onClick={() => buyCard(2)}>Buy Level 2</button>)}
-      {shells > 12 && (
-        <button onClick={() => buyCard(3)}>Buy Level 3</button>)}
+      {shells > 7 && level2cards > 0 &&(
+        <button onClick={() => buyCard(2)}>Buy Level 2 ({level2cards})</button>)}
+      {shells > 12 && level3cards > 0 && (
+        <button onClick={() => buyCard(3)}>Buy Level 3 ({level3cards})</button>)}
       <button
         onClick={() => {
           endTurn()

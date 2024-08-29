eb71400876900b68de54b7f9ccfbd6b8bbf5ddfb
nomadriding <fundora.a@gmail.com>
2024-08-29 19:10:14 -0400

    attack now saves all rewards of different types not just one type

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,14 +1,19 @@
 import React from "react"
 
-const Table = ({ table }) => {
+const Table = ({ table, buyCard }) => {
   return (
     <div className="tableContainer">
       {table.map((card, index) => {
         const halfPrice = Math.round(card.price / 2)
         return (
           <div key={index} className="card centerCard">
-            <div className="cardName">{card.name}</div>
-            {card.price && <div>Price: {halfPrice}</div>}
+            <div className="cardName">
+              {card.name}</div>
+            {card.price && <div>
+              <button onClick={() => buyCard(card)}>
+              Shells {halfPrice}
+              </button>
+              </div>}
           </div>
         )
       })}

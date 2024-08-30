a0b71045bc6ba3600c4450802bc6db9404890a6a
nomadriding <fundora.a@gmail.com>
2024-08-30 14:50:34 -0400

    adjust board alternating players, adjust starting deck names

--- src/Board.jsx
+++ src/Board.jsx
@@ -9,25 +9,29 @@ const Board = () => {
 
   return (
     <div>
-      <Base
-        player="Player 1"
-        turn={turn === 1}
-        table={table}
-        setTable={setTable}
-        endTurn={() => {
-          setTurn(2)
-        }}
-      />
       <Table table={table} />
-      <Base
-        player="Player 2"
-        turn={turn === 2}
-        table={table}
-        setTable={setTable}
-        endTurn={() => {
-          setTurn(1)
-        }}
-      />
+      {turn === 1 ? (
+        <Base
+          player="Player 1"
+          turn={turn === 1}
+          table={table}
+          setTable={setTable}
+          endTurn={() => {
+            setTurn(2)
+          }}
+        />
+      ) : (
+        <Base
+          player="Player 2"
+          turn={turn === 2}
+          table={table}
+          setTable={setTable}
+          endTurn={() => {
+            setTurn(1)
+          }}
+        />
+      )}
+  
     </div>
   )
 }
